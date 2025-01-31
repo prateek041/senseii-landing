b2c2b5e277db7298d955b061fe2cf948ed17b11e
Prateek Singh <prateeksingh9741@gmail.com>
2025-01-31 10:46:17 +0530

    web: how it works section

--- src/components/how.it.works.tsx
+++ src/components/how.it.works.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { CiStickyNote } from "react-icons/ci";
 import { Separator } from "./ui/separator";
+import { Badge } from "./ui/badge";
 
 const pageContent = [
   {
@@ -21,37 +22,40 @@ const pageContent = [
 
 export default function Working() {
   return (
-    <div className="container flex flex-col items-center justify-center text-center mx-auto">
-      <div className="flex flex-col gap-y-5">
-        <div className="gap-y-2">
-          <h4>How it works</h4>
-          <h3>
+    <div className="container flex flex-col w-full items-center justify-center text-center py-5 mx-auto">
+      <div className="flex flex-col w-full items-center justify-between gap-y-32">
+        <div className="space-y-2">
+          <Badge variant="outline">How it works</Badge>
+          <h3 className="md:text-4xl">
             {" "}
             Getting started with Senseii is simple! Here&apos;s how it works:
+            <Separator />
           </h3>
         </div>
-        <Image
-          className=""
-          src={"/mobile-demo.png"}
-          height={400}
-          width={400}
-          alt="running image of senseii on a mobile phone"
-        />
-        <div className="flex flex-col px-4">
-          {pageContent.map((item, index) => {
-            return (
-              <div key={index} className="flex">
-                <div className="">
-                  <CiStickyNote className="text-3xl border rounded-sm" />
+        <div className="md:flex">
+          <Image
+            className="md:h-[700px] md:w-[700px]"
+            src={"/mobile-demo.png"}
+            height={400}
+            width={400}
+            alt="running image of senseii on a mobile phone"
+          />
+          <div className="flex flex-col justify-start px-4">
+            {pageContent.map((item, index) => {
+              return (
+                <div key={index} className="flex md:my-2 md:gap-x-2">
+                  <div className="">
+                    <CiStickyNote className="md:text-4xl text-3xl border rounded-sm" />
+                  </div>
+                  <div className="text-start md:space-y-5">
+                    <h5 className="md:text-2xl">Step {index + 1}:</h5>
+                    <p className="md:text-lg">{item.description}</p>
+                    <Separator className="my-4" />
+                  </div>
                 </div>
-                <div className="text-start">
-                  <h5>Step {index}:</h5>
-                  <p>{item.description}</p>
-                  <Separator className="my-4" />
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
